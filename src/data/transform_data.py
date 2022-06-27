c165d564dfb8ee5d1979f64de32a642136f9e414
wilmerrenteria1960 <102265683+wilmerrenteria1960@users.noreply.github.com>
2022-06-27 16:15:05 -0500

    update_transform_data.py

--- src/data/transform_data.py
+++ src/data/transform_data.py
@@ -7,10 +7,29 @@ def transform_data():
     H23.
 
     """
-    raise NotImplementedError("Implementar esta función")
+    import os
+    import pandas as pd
+
+    archivoslanding = []
+    for files in os.listdir("/data_lake/landing"):
+        if files.endswith(".xls") or files.endswith(".xlsx"):
+            archivoslanding.append("/content/data_lake/landing/" + files)
+
+    for archivo in archivoslanding:
+        df = pd.read_excel(archivo,index_col=None, header=None)
+        indexDF = df.index[df.iloc[:, 0] == 'Fecha'].tolist()
+        dffinal = df.iloc[indexDF[0]+1:,0:25]
+        dffinal.columns = ["Fecha","H00","H01","H02","H03","H04","H05","H06","H07","H08","H09","H10","H11","H12","H13","H14","H15","H16","H17","H18","H19","H20","H21","H22","H23"]
+        dffinal['Fecha'] =  dffinal[['Fecha']].apply(pd.to_datetime)
+        dffinal['Fecha'] = dffinal['Fecha'].dt.date
+        nombreExtension = archivo.split("/")[-1]
+        nombreCSV = nombreExtension.split(".")[0] + ".csv"
+        df.to_csv("data_lake/raw/" + nombreCSV, index=False)
+
+    #raise NotImplementedError("Implementar esta función")
 
 
 if __name__ == "__main__":
     import doctest
-
+    transform_data()
     doctest.testmod()
