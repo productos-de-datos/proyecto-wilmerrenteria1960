b392b50a33f909c81ba7b3da4cedf691b62efd68
wilmerrenteria1960 <102265683+wilmerrenteria1960@users.noreply.github.com>
2022-06-27 13:17:37 -0500

    update ingestion_date.py

--- src/data/ingest_data.py
+++ src/data/ingest_data.py
@@ -13,10 +13,33 @@ def ingest_data():
     descarga debe realizarse usando únicamente funciones de Python.
 
     """
-    raise NotImplementedError("Implementar esta función")
+from urllib import request
+
+#Años a descargar
+anioDescargar = list(range(1995,2022))
+
+for anio in anioDescargar:
+  if anio==2016 or anio ==2017:
+      f = open(f"data_lake/landing/{anio}.xls", "wb")
+      f.write(
+          request.urlopen(
+              f"https://github.com/jdvelasq/datalabs/raw/master/datasets/precio_bolsa_nacional/xls/{anio}.xls"
+          ).read()
+      )
+      f.close()
+  else:
+      f = open(f"data_lake/landing/{anio}.xlsx", "wb")
+      f.write(
+          request.urlopen(
+              f"https://github.com/jdvelasq/datalabs/raw/master/datasets/precio_bolsa_nacional/xls/{anio}.xlsx"
+          ).read()
+      )
+      f.close()
+
+    #raise NotImplementedError("Implementar esta función")
 
 
 if __name__ == "__main__":
     import doctest
-
+    ingest_data()
     doctest.testmod()
