acab09356c0dfb734afac219bd1c2dd28c8b94b5
wilmerrenteria1960 <102265683+wilmerrenteria1960@users.noreply.github.com>
2022-06-27 21:03:11 -0500

    update

--- src/data/pipeline.py
+++ src/data/pipeline.py
@@ -11,12 +11,45 @@ En luigi llame las funciones que ya creo.
 
 
 """
+import ingest_data
+import transform_data
+import clean_data
+import compute_daily_prices
+import compute_monthly_prices
+import luigi
+from luigi import Task, LocalTarget
 
-if __name__ == "__main__":
 
-    raise NotImplementedError("Implementar esta función")
+class TareasRealizadasPrograma(Task):
+    def output(self):
+        return LocalTarget("data_lake/cleansed/precios-horarios.csv")
+
+    def run(self):
+        ingest_data.ingest_data()
+        transform_data.transform_data()
+        clean_data.clean_data()
+
+
+class Resultados(Task):
+    def requires(self):
+        return TareasRealizadasPrograma()
+
+    def output(self):
+        return LocalTarget(
+            [
+                "data_lake/business/precios-diarios.csv",
+                "data_lake/business/precios-mensuales.csv",
+            ]
+        )
+
+    def run(self):
+        compute_daily_prices.compute_daily_prices()
+        compute_monthly_prices.compute_monthly_prices()
+
 
 if __name__ == "__main__":
     import doctest
 
     doctest.testmod()
+    luigi.run(["Resultados", "--local-scheduler"])
+
